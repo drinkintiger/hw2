c1a9875b69c36cb45574591db154a0e5545cb545
Jordan <adsf>
2012-11-15 20:10:43 -0600

    starting point for hw

---
+++ tsp.c
@@ -0,0 +1,18 @@
+/*
+ * Jordan Harmel
+ * Vladimir Tsoy
+ * Nov. 15, 2012
+ *
+ * CS 470 : Traveling Salesperson
+ *
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(int argc, char * argv[]) {
+
+    File *file = fopen(
+
+}
