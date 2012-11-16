0565a436d80a96139a4f8449dedce937ed11ce90
drinkintiger <tsoy_vladimir@yahoo.com>
2012-11-15 22:07:38 -0600

    added some structs in tsp header file

--- tsp.h
+++ tsp.h
@@ -1,1 +1,15 @@
 #include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+typedef struct {
+    int *path;
+    int cost;
+} tour;
+
+typedef struct {
+    int cost;
+    int myCity;
+    struct node *children;
+} node;
