cfa39b7d13b5bf2f4e1cbff945abad15bf4e122c
Jordan <adsf>
2012-11-17 12:15:34 -0600

    commited because it wanted me to

--- tsp.c
+++ tsp.c
@@ -11,6 +11,10 @@
 
 FILE *file;
 
+int tour_finder(void);
+int feasible(struct tour curr_tour, int city);
+int tokenize_line(char *input);
+
 int main(int argc, char * argv[]) {
     tour best_tour;
     char line[64];
@@ -21,9 +25,27 @@ int main(int argc, char * argv[]) {
         printf("%s", line);
     }
     fclose(file);
+    
+    //after list is built
+    #pragma omp parallel num_threads(4)
+    {
+    tour_finder();
+    }
+    
     return 0;
 }
 
-int tokenize_line(char *input) {
+int tour_finder(void) {
+    int my_rank = omp_get_thread_num();
+    int thread_count = omp_get_num_threads();
+    printf("hello %d of %d", my_rank, thread_count);
+    return 0;
     
+}
+
+int feasible(struct tour curr_tour, int city) {
+    return 0;
+}
+int tokenize_line(char *input) {
+    return 0;
 }
